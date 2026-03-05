74b30f8348155ff6517c14c5fbda87f436f5beb0
goba617 <willtsai0611@gmail.com>
2026-03-05 16:50:12 +0800

    Refactor canvas setup and player object initialization

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
-const canvas = document.getElementById("game");
-const ctx = canvas.getContext("2d");
-
-ctx.fillStyle = "red";
-ctx.fillRect(100, 100, 50, 50);
+document.body.style.color="#000";
+let player={
+  x:100,
+  y:600,
+  width:40,
+  height:40
+};
+let playerPx=window.screen.width/2 - player.width/2;
+let canvas = document.getElementById("game");
+canvas.width = window.innerWidth;
+canvas.height = window.screen.height;
+let ctx = canvas.getContext("2d");
+ctx.strokeStyle = "#ffffff";
+ctx.lineWidth=2;
+main = ctx.strokeRect(playerPx,player.y, player.width, player.height);
+console.log(window.screen.height)
+canvas.addEventListener("pointerdown",function() {
+  let ifDown = true;
+});
+canvas.addEventListener("pointerup",function(){
+  let ifDown = false;
+});
+canvas.addEventListener("pointermove",function(e){
+  
+});
